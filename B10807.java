2f6443631aaa1d5245ce06bacdc9ae37aa85237f
kimajswl <ajswl4763@naver.com>
2025-04-08 22:31:01 +0900

    B10807

--- B10807.java
+++ B10807.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class B10807 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int[] arr = new int[n];
         int count = 0;
 
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = scanner.nextInt();
         }
 
-        int v = sc.nextInt();
+        int v = scanner.nextInt();
         for (int i = 0; i < n; i++) {
             if (arr[i] == v) {
                 count++;
