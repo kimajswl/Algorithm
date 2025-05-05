6f667de1ce2824fb1b7419d3d95398b245b8d376
kimajswl <ajswl4763@naver.com>
2025-05-05 12:04:44 +0900

    B10926

---
+++ B10926.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B10926 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String name = s.next();
+        System.out.print(name + "??!");
+    }
+}
